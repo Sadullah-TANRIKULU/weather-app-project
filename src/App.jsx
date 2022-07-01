a096568898112f1b19bc85b2ef720fba74f5e0ee
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-07-01 10:58:45 +0300

    try to implement tailwindcss in this project but not yet

--- src/App.jsx
+++ src/App.jsx
@@ -20,17 +20,17 @@ function App() {
       }
     }
     getApi();
-  }, [search]) // bu virgül ve köşeli parantez app ilk çalıştığında çağırması için konuldu, bu şekilde useEffect hook u diğer hook ve değişkenleri takip edebilir
+  }, [search]) // bu virgül ve köşeli parantez app ilk render dan sonra çağırması için konuldu, bu şekilde useEffect hook u diğer hook ve değişkenleri takip edebilir
 
   return (
-  <div className="App">
-    <div>
+  <div className="flex flex-col items-center h-screen bg-gray-200">
+    <div className='flex flex-col items-center bg-amber-100 p-4 rounded-lg '>
       <div className="mb-3 pt-0">
         <input 
-        onKeyDown={(e)=> setSearch(e.target.value)}   // onChange yerine onKeyDown koydum api sınırını hızla aşmamak için, yani her harfe basınca değil yazıp enterlayınca api ye get request yapsın diye
+        onKeyDown={(e)=> setSearch(e.target.value)}   // onChange her değişikliği API den çağırır, public free api sınırını hızla tüketmiş oluruz, bunun yerine yazıp tıklayınca veya enter layınca çalışan onSubmit event ları daha avantajlı olur
         type="text"
         placeholder="Placeholder"
-        className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"/>
+        className="bg-red-200"/>
       </div>
       { city && <City city={city}/> } {/* short circuit */}
     </div>
--- src/City.jsx
+++ src/City.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const City = ({ city }) => {
     console.log(city);
     return (
-        <div>
+        <div className='flex flex-col items-center h-screen bg-gray-200' >
             <div className='w-[350px] rounded-md shadow mx-auto'>
                 <h1 className='text-5xl font-bold'>{city.main.temp}</h1>
                 <h1 className='text-3xl font-semibold'>{city.name}</h1>
